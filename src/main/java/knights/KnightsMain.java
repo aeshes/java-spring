ef95b20a1e4a02624169d44c2138bae7e2697080
aeshes <mathesis@inbox.ru>
2018-07-01 14:59:01 +0300

    init

--- src/main/java/knights/KnightsMain.java
+++ src/main/java/knights/KnightsMain.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class KnightsMain {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("META-INF/spring/knight.xml");
+                new ClassPathXmlApplicationContext("main/resources/knight.xml");
         Knight knight = context.getBean(Knight.class);
         knight.embarkOnQuest();
         context.close();
